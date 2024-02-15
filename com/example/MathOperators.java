acc7ee328898ddb4ad3640955e4a5b9fd4e8c129
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-02-15 00:25:24 +0000

    More fixes

--- com/example/MathOperators.java
+++ com/example/MathOperators.java
@@ -8,139 +8,112 @@ import java.util.Scanner;
  *
  * @author Christopher Di Bert
  * @version 1.0
- * @since 14-2-2024
+ * @since 2024-02-14
  */
-
-
 public final class MathOperators {
 
-  /** Private constructor to prevent instantiation. */
-  private MathOperators() {
-    throw new UnsupportedOperationException("Cannot instantiate");
-  }
-
-  // Properties to be accessed by calculator methods
-  private static int userDecimalPlaces = 0;
-  private static Double num1 = 0.0d;
-  private static Double num2 = 0.0d;
-  private static String userOperation;
-  private static final String[] OPERATIONS = {"+", "-", "*", "/", "^", "sqrt"};
-
-  public static void main(final String[] args) {
-    System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    System.out.println("Hello! Welcome to the calculator!");
-
-    /*Gets the user's input, if there weren't any error messages returned,
-     * it calls the math function. If not, prints the error message.
-     */
-    final String errorMessage = GetUserValues();
-    if (errorMessage.equals("No errors")) {
-      System.out.println("Answer: " + Arithmetic
-      (num1, num2, userDecimalPlaces, userOperation));
-    } else {
-      System.out.println(errorMessage);
-    }
-  }
-
-  // Used to check if user's operator actually exists
-  private static boolean isValidOperation(String userOperation) {
-    // Foreach loop iterates through all possible operations
-    for (String operation : OPERATIONS) {
-      if (userOperation.equals(operation)) {
-        return true;
-      }
-    }
-    // User entered invalid operation
-    return false;
-  }
-
-  // Method used to retrieve the user's input'
-  private static String GetUserValues() {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter how many decimal places you want in your answers: ");
-    try {
-      userDecimalPlaces = sc.nextInt();
-    } catch (Exception e) {
-      return "You must only enter a whole number!";
+    /** Private constructor to prevent instantiation. */
+    private MathOperators() {
+        throw new UnsupportedOperationException("Cannot instantiate");
     }
 
-    // Call clears the input buffer to prevent input not being retrieved
-    sc.nextLine();
+    private static int userDecimalPlaces = 0;
+    private static Double num1 = 0.0d;
+    private static Double num2 = 0.0d;
+    private static String userOperation;
+    private static final String[] OPERATIONS = {"+", "-", "*", "/", "^", "sqrt"};
 
-    // Asks the user for the operation they want to use
-    System.out.print("Enter the operation you wish to perform\n");
-    System.out.print("(+, -, *, /, ^, sqrt)\n>> ");
-    userOperation = sc.nextLine();
-    if (isValidOperation(userOperation) == false) {
-      return "You must enter a listed operator!";
+    public static void main(final String[] args) {
+        System.out.print("\n".repeat(20));
+        System.out.println("Hello! Welcome to the calculator!");
+
+        final String errorMessage = getUserValues();
+        if ("No errors".equals(errorMessage)) {
+            System.out.println("Answer: " + arithmetic(num1, num2, userDecimalPlaces, userOperation));
+        } else {
+            System.out.println(errorMessage);
+        }
     }
 
-    /*Gets numbers from the user. If the user wants to square root,
-     * just tells them to enter one number.
-     */
-    if (userOperation.charAt(0) != 's') {
-      System.out.print("Enter num 1: ");
-      try {
-        num1 = sc.nextDouble();
-        System.out.print("Enter num 2: ");
-        num2 = sc.nextDouble();
-      } catch (Exception e) {
-        return "You must only enter numbers!";
-      }
-    } else {
-      System.out.print("Enter a number to square root: ");
-      try {
-        num1 = sc.nextDouble();
-      } catch (Exception e) {
-        return "You must enter a valid number!";
-      }
+    private static boolean isValidOperation(final String userOperation) {
+        for (final String operation : OPERATIONS) {
+            if (userOperation.equals(operation)) {
+                return true;
+            }
+        }
+        return false;
     }
-    // Returns any error messages or lack thereof
-    return ErrorCheck();
-  }
 
-  // Function used to catch any mathematically impossible operations
-  private static String ErrorCheck() {
-    // Checks for division by 0
-    if (num2.intValue() == 0 && userOperation.equals("/")) {
-      return "You cannot divide by zero!";
-    } else if (num1 < 0 && userOperation.equals("sqrt")) {
-      return "You can't take the square root of a negative number!";
+    private static String getUserValues() {
+        final Scanner sc = new Scanner(System.in);
+        System.out.print("Enter how many decimal places you want in your answers: ");
+        try {
+            userDecimalPlaces = sc.nextInt();
+        } catch (final Exception e) {
+            return "You must only enter a whole number!";
+        }
+
+        sc.nextLine();
+
+        System.out.print("Enter the operation you wish to perform\n");
+        System.out.print("(+, -, *, /, ^, sqrt)\n>> ");
+        userOperation = sc.nextLine();
+        if (!isValidOperation(userOperation)) {
+            return "You must enter a listed operator!";
+        }
+
+        if (userOperation.charAt(0) != 's') {
+            System.out.print("Enter num 1: ");
+            try {
+                num1 = sc.nextDouble();
+                System.out.print("Enter num 2: ");
+                num2 = sc.nextDouble();
+            } catch (final Exception e) {
+                return "You must only enter numbers!";
+            }
+        } else {
+            System.out.print("Enter a number to square root: ");
+            try {
+                num1 = sc.nextDouble();
+            } catch (final Exception e) {
+                return "You must enter a valid number!";
+            }
+        }
+        return errorCheck();
     }
-    // If no errors were found, returns string saying no errors found
-    return "No errors";
-  }
 
-  // Method used to do math and rounding. Returns a string that can be printed.
-  private static String Arithmetic(Double num1, Double num2, int decimalPlaces, String operation) {
-    // Sets the decimal precision of the decimal formatter.
-    DecimalFormat decimalFormatter = new DecimalFormat("0." + ("0".repeat(decimalPlaces)));
-    Double result;
-    switch (operation) {
-      case "+":
-        result = num1 + num2;
-        break;
-
-      case "-":
-        result = num1 - num2;
-        break;
-
-      case "*":
-        result = num1 * num2;
-        break;
-
-      case "/":
-        result = num1 / num2;
-        break;
-
-      case "^":
-        result = Math.pow(num1, num2);
-        break;
-
-      default:
-        result = Math.sqrt(num1);
-        break;
+    private static String errorCheck() {
+        if (num2.intValue() == 0 && userOperation.equals("/")) {
+            return "You cannot divide by zero!";
+        } else if (num1 < 0 && userOperation.equals("sqrt")) {
+            return "You can't take the square root of a negative number!";
+        }
+        return "No errors";
     }
-    return decimalFormatter.format(result);
-  }
+
+    private static String arithmetic(final Double num1, final Double num2, final int decimalPlaces, final String operation) {
+        final DecimalFormat decimalFormatter = new DecimalFormat("0." + "0".repeat(decimalPlaces));
+        final Double result;
+        switch (operation) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            case "^":
+                result = Math.pow(num1, num2);
+                break;
+            default:
+                result = Math.sqrt(num1);
+                break;
+        }
+        return decimalFormatter.format(result);
+    }
 }
